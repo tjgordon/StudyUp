1c365cd4bb5cefdeb86a1c6ff5a2a1615465f4df
Anastasia Levchenko <alevchenko@ucdavis.edu>
2019-02-09 18:10:53 -0800

    finishing all of the tests

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -95,24 +95,22 @@ class EventServiceImplTest {
 		}); 
 	}
 	
-
-	
-	// Ideas:
-	// - Returns event as active when it isn't / vv
-	// - Not even checking if event is active
-	
-	@Test 
-	void testGetActiveEvents_notCheckingActive_badCase() {
-
-		// Compare to empty list
-		List<Event> futureEventList = new ArrayList<>();
-
+	@Test
+	void testUpdateEventName_NameLength20_goodCase() throws StudyUpException{
+		int eventId = 1; 
+		String name = "This has length 20!!"; 
+		eventServiceImpl.updateEventName(eventId, name); 
+		assertEquals(name, DataStorage.eventData.get(eventId).getName());
 		
-		// Should fail because no checking for past events is done, and the only default event should be in the past
-		Assertions.assertEquals(eventServiceImpl.getActiveEvents(), futureEventList);
 	}
 	
-	
+	@Test 
+	void testGetActiveEvents_notCheckingActive_goodCase() {
+		List<Event> futureEventList = new ArrayList<>(); 
+		// Should fail because no checking for past events is done, and the
+		// only event in the list is in the past
+		Assertions.assertEquals(eventServiceImpl.getActiveEvents(), futureEventList);
+	}
 	
 	//this test has a mixture of future and past events, and we only want to
 	//return the events that are in the future. 
@@ -141,6 +139,7 @@ class EventServiceImplTest {
 	@Test
 	void testGetActiveEvents_OnlyFutureEvents_goodCase() {
 		DataStorage.eventData.clear(); //clear the data storage to be able to add future events
+		
 		Event event1 = new Event(); 
 		event1.setEventID(1); 
 		event1.setDate(new Date(2020, 3, 23));
@@ -162,7 +161,142 @@ class EventServiceImplTest {
 		activeEvents.add(event2); 
 		
 		Assertions.assertEquals(activeEvents, eventServiceImpl.getActiveEvents());
-
 	}
+	
+	@Test 
+	void testGetPastEvents_goodCase() {
+		int eventID = 1; 
+		List<Event> pastEvents = new ArrayList<>(); 
+		pastEvents.add(DataStorage.eventData.get(eventID)); 
+		
+		Assertions.assertEquals(pastEvents, eventServiceImpl.getPastEvents()); 
+	}
+	
+	@Test
+	void testGetPastEvents_hasAFutureEvent_goodCase() {
+		// add a future event to data storage to test that only the 
+		// future events are returned in this method
+		int pastEventID = 1; 
+		Event event1 = new Event(); 
+		event1.setEventID(2); 
+		event1.setDate(new Date(2020, 3, 23));
+		event1.setName("Event 2");
+		Location location = new Location(-122, 37);
+		event1.setLocation(location);
+		
+		DataStorage.eventData.put(event1.getEventID(), event1); 
+		
+		List<Event> pastEvents = new ArrayList<>();
+		pastEvents.add(DataStorage.eventData.get(pastEventID)); 
+		
+		Assertions.assertEquals(pastEvents, eventServiceImpl.getPastEvents());	
+	}	
+	
+	@Test
+	void testAddStudentToEvent_addOneStudent_goodCase() throws StudyUpException{
+		int eventID = 1; 
+		
+		Student student = DataStorage.eventData.get(eventID).getStudents().get(0); 
+		// create the new student that will be added to the event
+		Student student2 = new Student();
+		student2.setFirstName("Jane");
+		student2.setLastName("Doe");
+		student2.setEmail("JaneDoe@email.com");
+		student2.setId(2);
 
+		List<Student> eventStudents = new ArrayList<>();
+		eventStudents.add(student);
+		eventStudents.add(student2); 
+		
+		
+		Assertions.assertEquals(eventStudents, eventServiceImpl.addStudentToEvent(student2, eventID).getStudents());
+	}
+	
+	// want to throw and exception when trying to add students
+	// to an event that already has two students in it -- each event is 
+	// only supposed to have a max of two students
+	@Test 
+	void testAddStudentToEvent_moreThan2StudentsAdded_badCase() throws StudyUpException {
+		int eventID = 1; 
+		
+		Student student2 = new Student();
+		student2.setFirstName("Jane");
+		student2.setLastName("Doe");
+		student2.setEmail("JaneDoe@email.com");
+		student2.setId(2);
+		
+		Student student3 = new Student();
+		student2.setFirstName("Jacob");
+		student2.setLastName("Doe");
+		student2.setEmail("JacobDoe@email.com");
+		student2.setId(3);
+		
+		eventServiceImpl.addStudentToEvent(student2, eventID); 
+		
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(student3, eventID); 
+		}); 
+		
+	}
+	
+	//test that an exception is thrown when you try to query an event
+	// that does not exist
+	@Test 
+	void testAddStudentToEvent_nullEvent_badCase() {
+		int eventID = 3; 
+		
+		Student student2 = new Student();
+		student2.setFirstName("Jane");
+		student2.setLastName("Doe");
+		student2.setEmail("JaneDoe@email.com");
+		student2.setId(2);
+		
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(student2, eventID); 
+		}); 
+	}
+		
+	
+	// test that adding a student to an event with no students in it 
+	// currently works as expected.
+	@Test 
+	void testAddStudentToEvent_emptyEvent_goodCase() throws StudyUpException{
+		// create a new event, with no students in that event
+		int eventID = 2; 
+		Event event = new Event();
+		event.setEventID(1);
+		event.setDate(new Date());
+		event.setName("Event 1");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		
+		Student student2 = new Student();
+		student2.setFirstName("Jane");
+		student2.setLastName("Doe");
+		student2.setEmail("JaneDoe@email.com");
+		student2.setId(2);
+		
+		DataStorage.eventData.put(eventID, event); 
+		List<Student> students = new ArrayList<>(); 
+		students.add(student2); 
+		
+		Assertions.assertEquals(students,
+				eventServiceImpl.addStudentToEvent(student2, eventID).getStudents()); 
+	}
+	
+	@Test
+	void testDeleteEvent_goodCase() {
+		int eventID = 1; 
+		eventServiceImpl.deleteEvent(eventID); 
+		// check that the event storage is empty
+		Assertions.assertEquals(0, DataStorage.eventData.size()); 
+	}
+	
+	@Test 
+	void testDeleteEvent_eventDoesNotExist_badCase() {
+		int eventID = 4; 
+		Assertions.assertEquals(null,
+				eventServiceImpl.deleteEvent(eventID)); 
+	}
+	
 }
