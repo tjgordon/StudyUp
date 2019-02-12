162fcb4cb79caca2c80efc77a61809f398b67d6f
Anastasia Levchenko <alevchenko@ucdavis.edu>
2019-02-11 20:28:30 -0800

    final set of tests

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -75,8 +75,11 @@ class EventServiceImplTest {
 			eventServiceImpl.updateEventName(eventID, "Renamed Event 3");
 		  });
 	}
-	
 
+	
+	// we believe there should be a test case checking to see whether 
+	// the new name is empty or not 
+	// this test is supposed to fail - because there is no checker currently
 	@Test
 	void testUpdateEventName_emptyName_badCase() {
 		int eventID = 1; 
@@ -95,6 +98,9 @@ class EventServiceImplTest {
 		}); 
 	}
 	
+	// the bug we discovered for the updateEventName() method
+	// we are supposed to allow names of length 20, but we don't 
+	// so this test fails
 	@Test
 	void testUpdateEventName_NameLength20_goodCase() throws StudyUpException{
 		int eventId = 1; 
@@ -104,23 +110,31 @@ class EventServiceImplTest {
 		
 	}
 	
+	
+	// the bug we found in getActiveEvenst() is that the method does not 
+	// check whether the event is really in the future
+	// so this test should fail, since there are actually no events that are 
+	// in the future
 	@Test 
 	void testGetActiveEvents_notCheckingActive_goodCase() {
 		List<Event> futureEventList = new ArrayList<>(); 
 		// Should fail because no checking for past events is done, and the
 		// only event in the list is in the past
-		Assertions.assertEquals(eventServiceImpl.getActiveEvents(), futureEventList);
+		Assertions.assertEquals(futureEventList, eventServiceImpl.getActiveEvents());
 	}
 	
-	//this test has a mixture of future and past events, and we only want to
-	//return the events that are in the future. 
+	// this test has a mixture of future and past events, and we only want to
+	// return the events that are in the future. 
+	// however, because the method does not check whether an event is in the future, 
+	// the test method fails
 	@Test 
 	void testGetActiveEvents_NotAllEventsFuture_badCase() {
 		// add an event that is in the future and add it to the data storage
 		Event futureEvent = new Event(); 
 		futureEvent.setEventID(2); 
 		//using deprecated constructor for ease of creation
-		futureEvent.setDate(new Date(2020, 3, 23));
+		// date == 8/12/2020
+		futureEvent.setDate(new Date(1597215600000L));
 		futureEvent.setName("Event 2");
 		Location location = new Location(-122, 37);
 		futureEvent.setLocation(location);
@@ -140,16 +154,19 @@ class EventServiceImplTest {
 	void testGetActiveEvents_OnlyFutureEvents_goodCase() {
 		DataStorage.eventData.clear(); //clear the data storage to be able to add future events
 		
+		//  both of the events below are in the future
 		Event event1 = new Event(); 
 		event1.setEventID(1); 
-		event1.setDate(new Date(2020, 3, 23));
+		// date == 8/12/2020
+		event1.setDate(new Date(1597215600000L));
 		event1.setName("Event 1");
 		Location location = new Location(-122, 37);
 		event1.setLocation(location);
 		
 		Event event2 = new Event(); 
 		event2.setEventID(2); 
-		event2.setDate(new Date(2020, 3, 24));
+		// date == 8/12/2020
+		event2.setDate(new Date(1597215600000L));
 		event2.setName("Event 2");
 		event2.setLocation(location);
 		
@@ -163,6 +180,9 @@ class EventServiceImplTest {
 		Assertions.assertEquals(activeEvents, eventServiceImpl.getActiveEvents());
 	}
 	
+	
+	// the data storage just has one event, and it is in the past
+	// so that event should be the only one that the method returns
 	@Test 
 	void testGetPastEvents_goodCase() {
 		int eventID = 1; 
@@ -172,14 +192,20 @@ class EventServiceImplTest {
 		Assertions.assertEquals(pastEvents, eventServiceImpl.getPastEvents()); 
 	}
 	
+	
+	// the data storage now has a mixture of events both in the past and in the future
+	// we want to make sure that only the events that are in the past are being returned
 	@Test
 	void testGetPastEvents_hasAFutureEvent_goodCase() {
 		// add a future event to data storage to test that only the 
-		// future events are returned in this method
-		int pastEventID = 1; 
+		// past events are returned in this method
+		int pastEventID = 1; // this is the event that is already in the DataStorage
+		
+		// now creating an event that is in the future, and making sure it isn't returned
 		Event event1 = new Event(); 
 		event1.setEventID(2); 
-		event1.setDate(new Date(2020, 3, 23));
+		// date == 8/12/2020
+		event1.setDate(new Date(1597215600000L));
 		event1.setName("Event 2");
 		Location location = new Location(-122, 37);
 		event1.setLocation(location);
@@ -192,23 +218,28 @@ class EventServiceImplTest {
 		Assertions.assertEquals(pastEvents, eventServiceImpl.getPastEvents());	
 	}	
 	
-	
+	// add another event that is in the past, 
 	@Test
 	void testGetPastEvents_addPastEvent_badCase() {
 		// Add a past event to data storage to test if past events are returned
 		// correctly.
-		int pastEventID = 2; 
+		int pastEventID = 1; // the ID of the original event in the past
+		
+		int newEventID = 2; 
 		Event event1 = new Event(); 
-		event1.setEventID(pastEventID); 
-		event1.setDate(new Date(1995, 3, 23));
+		event1.setEventID(newEventID); 
+		// date = 01/28/1970
+		event1.setDate(new Date(2323223232L));
 		event1.setName("Event 2");
 		Location location = new Location(-122, 37);
 		event1.setLocation(location);
 		
-		DataStorage.eventData.put(event1.getEventID(), event1); 
+		DataStorage.eventData.put(newEventID, event1); 
 		
 		List<Event> pastEvents = new ArrayList<>();
+		
 		pastEvents.add(DataStorage.eventData.get(pastEventID)); 
+		pastEvents.add(DataStorage.eventData.get(newEventID)); 
 		
 		Assertions.assertEquals(pastEvents, eventServiceImpl.getPastEvents());	
 	}	
