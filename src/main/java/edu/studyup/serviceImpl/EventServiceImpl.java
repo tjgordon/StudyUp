c3554dda8323819c3d4f410e3e604fd254d50277
Anastasia Levchenko <alevchenko@ucdavis.edu>
2019-02-20 17:21:18 -0800

    fixed 2 spotbugs bugs

--- src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
+++ src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
@@ -34,8 +34,8 @@ public class EventServiceImpl implements EventService {
 		Map<Integer, Event> eventData = DataStorage.eventData;
 		List<Event> activeEvents = new ArrayList<>();
 		
-		for (Integer key : eventData.keySet()) {
-			Event ithEvent= eventData.get(key);
+		for (Map.Entry<Integer, Event> entry : eventData.entrySet()) {
+			Event ithEvent= entry.getValue(); 
 			activeEvents.add(ithEvent);
 		}
 		return activeEvents;
@@ -46,8 +46,8 @@ public class EventServiceImpl implements EventService {
 		Map<Integer, Event> eventData = DataStorage.eventData;
 		List<Event> pastEvents = new ArrayList<>();
 		
-		for (Integer key : eventData.keySet()) {
-			Event ithEvent= eventData.get(key);
+		for (Map.Entry<Integer, Event> entry : eventData.entrySet()) {
+			Event ithEvent= entry.getValue();			
 			// Checks if an event date is before today, if yes, then add to the past event list.
 			if(ithEvent.getDate().before(new Date())) {
 				pastEvents.add(ithEvent);
